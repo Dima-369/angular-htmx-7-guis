c66063d9447d20f1c896856e8e143e39726c6863
Dima Butemann <dbutemann@gmail.com>
2023-11-07 21:28:23 +0100

    Remove unused code

--- spring-boot/src/main/java/com/seven/guis/springboot/Application.java
+++ spring-boot/src/main/java/com/seven/guis/springboot/Application.java
@@ -2,29 +2,16 @@ package com.seven.guis.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Application.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
-		app.run(args);
-	}
-
-//	@GetMapping("/counter")
-//	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-//		return String.format("%s!", name);
-//		Map<String, Object> model;
-//		model.put("title", "Hello Vue!");
-//		return new ModelAndView("counter.html", model);
-//	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+        app.run(args);
+    }
 }
