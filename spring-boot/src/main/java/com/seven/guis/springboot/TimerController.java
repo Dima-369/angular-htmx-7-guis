6851b978e38e542901f61c1dbd0c4a65a594d9a0
Dima Butemann <dbutemann@gmail.com>
2023-11-08 20:30:10 +0100

    Add HTML for TimerController based on Angular

--- spring-boot/src/main/java/com/seven/guis/springboot/TimerController.java
+++ spring-boot/src/main/java/com/seven/guis/springboot/TimerController.java
@@ -11,16 +11,50 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class TimerController {
 
+    private static String formatElapsed(int elapsedMs) {
+        double elapsedSeconds = (double) elapsedMs / 1000;
+        return String.format("%.1fs", elapsedSeconds);
+    }
+
+    // todo function to set models instead of copy/paste?
+
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @RequestMapping(value = "/timer", method = {RequestMethod.GET, RequestMethod.POST})
     public String timer(
             @RequestParam(value = "maxTimeMs", defaultValue = "10000") int maxTimeMs,
             @RequestParam(value = "durationMs", defaultValue = "5000") int durationMs,
             @RequestParam(value = "elapsedMs", defaultValue = "0") int elapsedMs,
-            @RequestParam(value = "totalTime", defaultValue = "27.03.2014") String to,
+            @RequestParam(value = "progressValueMs", defaultValue = "0") int progressValueMs,
             Model model) {
 
-        // todo how to do the timestamps?
-        return "oimero";
+        // todo how to do the last timestamp calculation?
+        model.addAttribute("maxTimeMs", maxTimeMs);
+        model.addAttribute("durationMs", durationMs);
+        model.addAttribute("elapsedMs", elapsedMs);
+        model.addAttribute("progressValueMs", progressValueMs);
+        model.addAttribute("elapsedFormatted", formatElapsed(elapsedMs));
+        return "timer";
     }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @RequestMapping(value = "/timer-reset", method = RequestMethod.POST)
+    public String timerReset(
+            @RequestParam(value = "maxTimeMs", defaultValue = "10000") int maxTimeMs,
+            @RequestParam(value = "durationMs", defaultValue = "5000") int durationMs,
+            @RequestParam(value = "progressValueMs", defaultValue = "0") int progressValueMs,
+            Model model) {
+
+        // todo how to do the last timestamp calculation?
+        model.addAttribute("maxTimeMs", maxTimeMs);
+        model.addAttribute("durationMs", durationMs);
+        model.addAttribute("progressValueMs", progressValueMs);
+
+        final int elapsedMs = 0;
+        model.addAttribute("elapsedMs", elapsedMs);
+        model.addAttribute("elapsedFormatted", formatElapsed(elapsedMs));
+
+        return "timer";
+    }
+
+    // todo how to do the reset button?
 }
