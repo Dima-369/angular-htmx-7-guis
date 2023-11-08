f0c37a947701807525aedb64b7127a3f9995387c
Dima Butemann <dbutemann@gmail.com>
2023-11-08 10:05:42 +0100

    Finish Angular timer

--- angular/src/app/timer/timer.component.ts
+++ angular/src/app/timer/timer.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css']
 })
-export class TimerComponent {
+export class TimerComponent implements OnInit {
 
+  /**
+   * The max time in milliseconds the timer can run.
+   */
+  maxTimeMs: number = 10000
+
+  /**
+   * From 0 to maxTimeMs.
+   */
+  durationMs: number = 5000
+
+  elapsedMs: number = 0
+
+  /**
+   * From 0 to maxTimeMs.
+   */
+  progressValueMs: number = 0;
+
+  private lastTimestamp: number = 0
+
+  updateOnAnimationFrame(timestamp: DOMHighResTimeStamp): void {
+    this.elapsedMs = Math.min(this.elapsedMs + (timestamp - this.lastTimestamp), this.durationMs);
+    this.progressValueMs = (this.elapsedMs / this.durationMs) * this.maxTimeMs;
+    this.lastTimestamp = timestamp;
+    requestAnimationFrame(this.updateOnAnimationFrame.bind(this))
+  }
+
+  formatElapsed(): string {
+    return (this.elapsedMs / 1000).toFixed(1) + "s"
+  }
+
+  ngOnInit(): void {
+    requestAnimationFrame(this.updateOnAnimationFrame.bind(this))
+  }
+
+  reset(): void {
+    this.elapsedMs = 0
+  }
+
+  updateDuration($event: Event): void {
+    this.durationMs = parseInt(($event.target as HTMLInputElement).value);
+  }
 }
