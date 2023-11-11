3da7da1e5b23f39bc2103587b59864e27f732eb4
Dima Butemann <dbutemann@gmail.com>
2023-11-11 15:24:48 +0100

    Revert back to port 8083 (since Port doesn't matter on render.com)

--- spring-boot/src/main/java/com/seven/guis/springboot/Application.java
+++ spring-boot/src/main/java/com/seven/guis/springboot/Application.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 public class Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "80"));
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
         app.run(args);
     }
 }
